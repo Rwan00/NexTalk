421efe53ca5dec299aaf081ef4c35ecd11cd4eb4
Rwan00 <93911923+Rwan00@users.noreply.github.com>
2025-11-14 20:12:48 +0200

    add app colors

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,35 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:nextalk/firebase_options.dart';
+import 'package:nextalk/services/navigation_service.dart';
 
 import 'package:nextalk/services/register_services.dart';
+import 'package:nextalk/theme/app_colors.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  registerServices();
+ 
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  runApp(const MyApp());
+   registerServices();
+  runApp(const NexTalk());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class NexTalk extends StatelessWidget {
+  const NexTalk({super.key});
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+    
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+       
+        colorScheme: ColorScheme.fromSeed(seedColor: AppColors.primaryColor),
+        scaffoldBackgroundColor: AppColors.backgroundColor,
+      
+        bottomNavigationBarTheme: BottomNavigationBarThemeData(backgroundColor: AppColors.secondaryColor,),
       ),
+      navigatorKey: NavigationService.navigatorKey,
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
---
+++ lib/theme/app_colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/cupertino.dart';
+
+class AppColors {
+  static Color backgroundColor = Color.fromRGBO(236, 244, 232,1);
+  static Color primaryColor = Color.fromRGBO(171, 231, 178,1);
+  static Color secondaryColor = Color.fromRGBO(203, 243, 187,1);
+}
