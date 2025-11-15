6f05c516915ab361504d4c969d177bc0935dbe18
Rwan00 <93911923+Rwan00@users.noreply.github.com>
2025-11-15 15:39:43 +0200

    add authentication provider

--- lib/main.dart
+++ lib/main.dart
@@ -2,18 +2,20 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:nextalk/firebase_options.dart';
+import 'package:nextalk/providers/authentication_provider.dart';
 import 'package:nextalk/routes/app_routes.dart';
 import 'package:nextalk/routes/pages_routes.dart';
 import 'package:nextalk/services/navigation_service.dart';
 
 import 'package:nextalk/services/register_services.dart';
 import 'package:nextalk/theme/app_colors.dart';
+import 'package:provider/provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
- 
+
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-   registerServices();
+  registerServices();
   runApp(const NexTalk());
 }
 
@@ -23,19 +25,25 @@ class NexTalk extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-    
-      theme: ThemeData(
-       
-        colorScheme: ColorScheme.fromSeed(seedColor: AppColors.kPrimaryColor),
-        scaffoldBackgroundColor: AppColors.kBackgroundColor,
-      textTheme: GoogleFonts.nunitoTextTheme(),
-        bottomNavigationBarTheme: BottomNavigationBarThemeData(backgroundColor: AppColors.kSecondaryColor,),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider<AuthenticationProvider>(
+          create: (context) => AuthenticationProvider(),
+        ),
+      ],
+      child: MaterialApp(
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: AppColors.kPrimaryColor),
+          scaffoldBackgroundColor: AppColors.kBackgroundColor,
+          textTheme: GoogleFonts.nunitoTextTheme(),
+          bottomNavigationBarTheme: BottomNavigationBarThemeData(
+            backgroundColor: AppColors.kSecondaryColor,
+          ),
+        ),
+        navigatorKey: NavigationService.navigatorKey,
+        initialRoute: PagesRoutes.kLoginPage,
+        routes: routes(),
       ),
-      navigatorKey: NavigationService.navigatorKey,
-      initialRoute: PagesRoutes.kLoginPage,
-  routes: routes(),
     );
   }
 }
-
