4bbee4c6c1bd9bfe09fce591ba0a276f03622600
Rwan00 <93911923+Rwan00@users.noreply.github.com>
2025-11-15 16:09:41 +0200

    add chat user model

---
+++ lib/models/chat_user_model.dart
@@ -0,0 +1,42 @@
+class ChatUserModel {
+  final String uid;
+  final String name;
+  final String email;
+  final String imageUrl;
+  final DateTime lastActive;
+
+  ChatUserModel({
+    required this.uid,
+    required this.name,
+    required this.email,
+    required this.imageUrl,
+    required this.lastActive,
+  });
+
+  factory ChatUserModel.fromJson(Map<String, dynamic> json) {
+    return ChatUserModel(
+      uid: json["uid"],
+      name: json["namw"],
+      email: json["email"],
+      imageUrl: json["image"],
+      lastActive: json["last_active"].toDate(),
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      "email": email,
+      "name": name,
+      "image": imageUrl,
+      "last_active": lastActive,
+    };
+  }
+
+  String lastDayActive() {
+    return "${lastActive.month}/${lastActive.day}/${lastActive.year}";
+  }
+
+  bool wasRecentlyActive() {
+    return DateTime.now().difference(lastActive).inHours < 2;
+  }
+}
