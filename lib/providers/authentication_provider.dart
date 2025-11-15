c5cdd5d675036b952a38c997d0e0510d9bdc8d9a
Rwan00 <93911923+Rwan00@users.noreply.github.com>
2025-11-15 23:11:49 +0200

    parsing user data

--- lib/providers/authentication_provider.dart
+++ lib/providers/authentication_provider.dart
@@ -3,6 +3,7 @@ import 'dart:developer';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
+import 'package:nextalk/models/chat_user_model.dart';
 import 'package:nextalk/services/database_service.dart';
 import 'package:nextalk/services/navigation_service.dart';
 
@@ -10,6 +11,7 @@ class AuthenticationProvider extends ChangeNotifier {
   late final FirebaseAuth _auth;
   late final NavigationService _navigationService;
   late final DatabaseService _databaseService;
+  late ChatUserModel chatUser;
 
   AuthenticationProvider() {
     _auth = FirebaseAuth.instance;
@@ -20,7 +22,17 @@ class AuthenticationProvider extends ChangeNotifier {
       if (user != null) {
         log("Logged In");
         _databaseService.updateUserLastSeenTime(user.uid);
-        _databaseService.getUser(user.uid);
+        _databaseService.getUser(user.uid).then((snapShot) {
+          Map<String, dynamic> userData =
+              snapShot.data()! as Map<String, dynamic>;
+          chatUser = ChatUserModel.fromJson({
+            "uid": user.uid,
+            "name": userData["name"],
+            "email": userData["email"],
+            "last_active": userData["last_active"],
+            "image": userData["image"],
+          });
+        });
       } else {
         log("Not Authenticated");
       }
