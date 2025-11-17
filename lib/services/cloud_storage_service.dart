e569c46fdb8bd1baebddfee6b87d36b8f1c88400
Rwan00 <93911923+Rwan00@users.noreply.github.com>
2025-11-17 22:28:33 +0200

    add save images to cloude storge method

--- lib/services/cloud_storage_service.dart
+++ lib/services/cloud_storage_service.dart
@@ -1,7 +1,40 @@
+import 'dart:developer';
+import 'dart:io';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:file_picker/file_picker.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 
 const String USER_COLLECTION = "Users";
 
 class CloudStorageService {
   final FirebaseStorage _storage = FirebaseStorage.instance;
+
+  Future<String?> saveUserImageToStorage(String uid, PlatformFile file) async {
+    try {
+      Reference ref = _storage.ref().child(
+        "images/users/$uid/profile.${file.extension}",
+      );
+      UploadTask task = ref.putFile(File(file.path!));
+      return await task.then((result) => result.ref.getDownloadURL());
+    } catch (e) {
+      log(e.toString());
+    }
+  }
+
+  Future<String?> saveChatImageToStorage(
+    String chatId,
+    String userId,
+    PlatformFile file,
+  ) async {
+    try {
+      Reference ref = _storage.ref().child(
+        "images/chats/$chatId/${userId}_${Timestamp.now().millisecondsSinceEpoch}.${file.extension}",
+      );
+       UploadTask task = ref.putFile(File(file.path!));
+      return await task.then((result) => result.ref.getDownloadURL());
+    } catch (e) {
+      log(e.toString());
+    }
+  }
 }
