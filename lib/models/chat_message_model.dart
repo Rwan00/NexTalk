a550e2c7147d7b801dca92181f3c917484a3f7f1
Rwan00 <93911923+Rwan00@users.noreply.github.com>
2025-11-19 00:42:26 +0200

    add chat message model

---
+++ lib/models/chat_message_model.dart
@@ -0,0 +1,55 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+enum MessageType { TEXT, IMAGE, UNKNOWN }
+
+class ChatMessageModel {
+  final MessageType type;
+  final String content;
+  final String senderId;
+  final DateTime sentTime;
+
+  ChatMessageModel({
+    required this.type,
+    required this.content,
+    required this.senderId,
+    required this.sentTime,
+  });
+
+  factory ChatMessageModel.fromJson(Map<String, dynamic> json) {
+    MessageType messageType;
+    switch (json["type"]) {
+      case "text":
+        messageType = MessageType.TEXT;
+      case "image":
+        messageType = MessageType.IMAGE;
+      default:
+        messageType = MessageType.UNKNOWN;
+        break;
+    }
+    return ChatMessageModel(
+      type: messageType,
+      content: json["content"],
+      senderId: json["sender_id"],
+      sentTime: json["sent_time"].toDate(),
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    String messageType;
+    switch (type) {
+      case MessageType.TEXT:
+        messageType = "text";
+      case MessageType.IMAGE:
+        messageType = "image";
+      default:
+        messageType = "";
+        break;
+    }
+    return {
+      "content":content,
+      "type":messageType,
+      "sender_id":senderId,
+      "sent_time":Timestamp.fromDate(sentTime),
+    };
+  }
+}
