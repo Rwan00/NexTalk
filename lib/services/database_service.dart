e18933faed265e3e63e9f89e505b9341dedbe58c
Rwan00 <93911923+Rwan00@users.noreply.github.com>
2025-11-21 18:46:08 +0200

    add CRUD functions to database

--- lib/services/database_service.dart
+++ lib/services/database_service.dart
@@ -1,6 +1,7 @@
 import 'dart:developer';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:nextalk/models/chat_message_model.dart';
 
 const String USER_COLLECTION = "users";
 const String CHAT_COLLECTION = "chats";
@@ -48,11 +49,48 @@ class DatabaseService {
         .get();
   }
 
+  Stream<QuerySnapshot> streamMessagesForChat(String chatId) {
+    return _db
+        .collection(CHAT_COLLECTION)
+        .doc(chatId)
+        .collection(MESSAGES_COLLECTION)
+        .orderBy("sent_time", descending: false)
+        .snapshots();
+  }
+
+  Future<void> addMessageToChat(String chatId, ChatMessageModel message) async {
+    try {
+      await _db
+          .collection(CHAT_COLLECTION)
+          .doc(chatId)
+          .collection(MESSAGES_COLLECTION)
+          .add(message.toJson());
+    } catch (e) {
+      log(e.toString());
+    }
+  }
+
+  Future<void> updateChatData(String chatId, Map<String, dynamic> data) async {
+    try {
+      await _db.collection(CHAT_COLLECTION).doc(chatId).update(data);
+    } catch (e) {
+      log(e.toString());
+    }
+  }
+
   Future<void> updateUserLastSeenTime(String uid) async {
     try {
       await _db.collection(USER_COLLECTION).doc(uid).update({
         "last_active": DateTime.now().toUtc(),
       });
+    } catch (e) {
+      log(e.toString());
+    }
+  }
+
+  Future<void> deleteChat(String chatId) async {
+    try {
+      await _db.collection(CHAT_COLLECTION).doc(chatId).delete();
     } catch (e) {
       log(e.toString());
     }
