63e3baa1b85517213a4e89bb52e8fee1264f29db
Rwan00 <93911923+Rwan00@users.noreply.github.com>
2025-11-23 16:04:26 +0200

    add users top bar

--- lib/pages/users_page.dart
+++ lib/pages/users_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:nextalk/providers/authentication_provider.dart';
+import 'package:nextalk/theme/app_colors.dart';
+import 'package:nextalk/widgets/top_bar.dart';
+import 'package:provider/provider.dart';
 
 class UsersPage extends StatefulWidget {
   const UsersPage({super.key});
@@ -8,8 +12,39 @@ class UsersPage extends StatefulWidget {
 }
 
 class _UsersPageState extends State<UsersPage> {
+  late double _deviceHeight;
+  late double _deviceWidth;
+
+  late AuthenticationProvider _auth;
+
+  final TextEditingController _searchController = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return  Scaffold();
+    _deviceHeight = MediaQuery.of(context).size.height;
+    _deviceWidth = MediaQuery.of(context).size.width;
+    _auth = Provider.of<AuthenticationProvider>(context);
+    return Container(
+      padding: EdgeInsets.symmetric(
+        horizontal: _deviceWidth * 0.03,
+        vertical: _deviceHeight * 0.02,
+      ),
+      height: _deviceHeight * 0.98,
+      width: _deviceWidth * 0.97,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          TopBar(
+            title: "Users",
+            primaryAction: IconButton(
+              onPressed: () {
+                _auth.logout();
+              },
+              icon: Icon(Icons.logout_outlined, color: AppColors.kPrimaryColor),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
