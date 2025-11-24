cef71dbde5217ad38c95d0aa354a67d99fa82e4f
Rwan00 <93911923+Rwan00@users.noreply.github.com>
2025-11-24 13:48:43 +0200

    add create chat to provider

--- lib/providers/users_page_provider.dart
+++ lib/providers/users_page_provider.dart
@@ -1,8 +1,11 @@
 import 'dart:developer';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
+import 'package:nextalk/models/chat_model.dart';
 import 'package:nextalk/models/chat_user_model.dart';
+import 'package:nextalk/pages/chat_page.dart';
 import 'package:nextalk/providers/authentication_provider.dart';
 import 'package:nextalk/services/database_service.dart';
 import 'package:nextalk/services/navigation_service.dart';
@@ -47,5 +50,40 @@ class UsersPageProvider extends ChangeNotifier {
       _selectedUsers.add(user);
     }
     notifyListeners();
+  }
+
+  void createChat() async {
+    try {
+      List<String> membersIds = _selectedUsers.map((user) => user.uid).toList();
+      membersIds.add(_auth.chatUser.uid);
+      bool isGroup = _selectedUsers.length > 1;
+      DocumentReference? doc = await _database.createChat({
+        "is_group": isGroup,
+        "is_activity": false,
+        "members": membersIds,
+      });
+
+      List<ChatUserModel> members = [];
+      for (var uid in membersIds) {
+        DocumentSnapshot userSnapshot = await _database.getUser(uid);
+        Map<String, dynamic> userData =
+            userSnapshot.data() as Map<String, dynamic>;
+        userData["uid"] = userSnapshot.id;
+        members.add(ChatUserModel.fromJson(userData));
+        ChatPage chatPage = ChatPage(
+          chatModel: ChatModel(
+            uid: doc!.id,
+            currentUserId: _auth.chatUser.uid,
+            isActivity: false,
+            isGroup: isGroup,
+            members: members,
+            messages: [],
+          ),
+        );
+        _navigation.navigateToPage(chatPage);
+      }
+    } catch (e) {
+      log(e.toString());
+    }
   }
 }
